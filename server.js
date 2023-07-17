61642d20e292f76b2dc46ac7939536972605231d
mrm15 <mrm.learn@gmail.com>
2023-07-18 00:50:54 +0330

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 // const mongoURI = process.env.DATABASE_URL || 'mongodb://localhost:27017/MyDb';
-const mongoURI = 'mongodb://127.0.0.1:27017/monotest';
-// const mongoURI = 'mongodb+srv://admin:admin@cluster0.samj2os.mongodb.net/?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb://127.0.0.1:27017/monotest';
+
+const mongoURI = `mongodb+srv://root:09384642159@cluster0.l53kbk3.mongodb.net/`;
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
-  // useUnifiedTopology: true
+  // useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -33,12 +34,11 @@ db.once('open', () => console.log('Database Connected'));
 
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-const authorRouter = require('./routes/authors');
-app.use('/authors', authorRouter);
-const bookRouter = require('./routes/books');
-app.use('/books', bookRouter);
+const productsRouter = require('./routes/products');
+app.use('/products', productsRouter);
 
-const port = process.env.PORT || 3600;
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
